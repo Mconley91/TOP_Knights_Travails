d1dfba59ff2d158e87ad9c9187eae82b8f8a6844
mconley91 <matthew.e.conley@gmail.com>
2025-04-28 14:52:20 -0400

    if statements consider the difference between target axis and current axis

--- lib/knight.rb
+++ lib/knight.rb
@@ -3,14 +3,15 @@ p 'knight file loaded'
 def knight_moves(current_step, target, path = [])
   return path if current_step == target
   path << current_step
-
+  x_axis_difference = target[0] - current_step[0]
+  y_axis_difference = target[1] - current_step[1]
   #incrementing toward target
-  if target[0] - current_step[0] > 2 && target[1] - current_step[1] > 2
-    knight_moves([current_step[0] + long_step[0], current_step[1] + long_step[1]], target, path)
+  if x_axis_difference > 0 && y_axis_difference > 0
+    # knight_moves([current_step[0] + long_step[0], current_step[1] + long_step[1]], target, path)
   end
   #decrementing toward target
-  if current_step[0] - target[0] > 2 && current_step[1] - target[1] > 2
-    knight_moves([current_step[0] - long_step[0], current_step[1] - long_step[1]], target, path)
+  if x_axis_difference < 0 && y_axis_difference < 0
+    # knight_moves([current_step[0] - long_step[0], current_step[1] - long_step[1]], target, path)
   end
   
   path
