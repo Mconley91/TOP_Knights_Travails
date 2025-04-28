9f327b6b5b640fab7d2d804a881ce608fed88910
mconley91 <matthew.e.conley@gmail.com>
2025-04-28 17:09:54 -0400

    still mulling over pathing logic

--- lib/knight.rb
+++ lib/knight.rb
@@ -1,19 +1,14 @@
 p 'knight file loaded'
 
 def knight_moves(current_step, target, path = [])
-  return path if current_step == target
-  path << current_step
   x_axis_difference = target[0] - current_step[0]
   y_axis_difference = target[1] - current_step[1]
-  #incrementing toward target
-  if x_axis_difference > 0 && y_axis_difference > 0
-    # knight_moves([current_step[0] + long_step[0], current_step[1] + long_step[1]], target, path)
-  end
-  #decrementing toward target
-  if x_axis_difference < 0 && y_axis_difference < 0
+  if x_axis_difference == 0 && y_axis_difference == 0
+    return path
+  else
+    path << current_step
     # knight_moves([current_step[0] - long_step[0], current_step[1] - long_step[1]], target, path)
   end
-  
   path
 end
 
@@ -67,5 +62,5 @@ end
 
 
 
-p knight_moves([7,7],[0,0])
+# p knight_moves([7,7],[0,0])
 p knight_moves([0,0],[7,7])
