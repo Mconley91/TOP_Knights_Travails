5213d25bef691f307fe7bcea23c2759f4219a6b7
mconley91 <matthew.e.conley@gmail.com>
2025-05-07 15:24:13 -0400

    test targets of 7,7 & 3,3 as well as their mirrors work. Algorithm struggles with asymmetrical coordinates

--- lib/knight.rb
+++ lib/knight.rb
@@ -6,30 +6,36 @@ def knight_moves(current_step, target, path = [])
   x_axis_value = current_step[0]
   y_axis_value = current_step[1]
   return if x_axis_value > 7 || y_axis_value > 7 || x_axis_value < 0 || y_axis_value < 0 #enforcing gameboard boundaries
-  if x_axis_difference == 0 && y_axis_difference == 0 # --------------arrived to the target
+  if x_axis_difference == 0 && y_axis_difference == 0 # arrived to the target
     path << [x_axis_value,y_axis_value]
-    # p path
-    # p "X:#{x_axis_value} Y:#{y_axis_value}"
     return path
   else
     path << [x_axis_value,y_axis_value]
     #-------------------------------------------------------finishing moves
     if x_axis_difference == 2 && y_axis_difference == 1
       knight_moves(move_2_oclock(current_step), target, path)
+      return
     elsif x_axis_difference == 2 && y_axis_difference == -1
       knight_moves(move_4_oclock(current_step), target, path)
+      return
     elsif x_axis_difference == -2 && y_axis_difference == 1
       knight_moves(move_10_oclock(current_step), target, path)
+      return
     elsif x_axis_difference == -2 && y_axis_difference == -1
       knight_moves(move_8_oclock(current_step), target, path)
+      return 
     elsif x_axis_difference == 1 && y_axis_difference == 2
-      knight_moves(move_1_oclock(current_step), target, path)
+      knight_moves(move_1_oclock(current_step), target, path) 
+      return
     elsif x_axis_difference == 1 && y_axis_difference == -2
       knight_moves(move_5_oclock(current_step), target, path)
+      return
     elsif x_axis_difference == -1 && y_axis_difference == 2
       knight_moves(move_11_oclock(current_step), target, path)
+      return
     elsif x_axis_difference == -1 && y_axis_difference == -2
       knight_moves(move_7_oclock(current_step), target, path)
+      return
     end
   end
   #------------------------------------------------------------ general increment/decrement logic
@@ -39,16 +45,16 @@ def knight_moves(current_step, target, path = [])
     elsif x_axis_value - 2 > 0 && y_axis_value + 1 < 7
       knight_moves(move_10_oclock(current_step), target, path)
     end
-    
-  # elsif x_axis_difference < -2 && y_axis_difference < -1
-  #   if y_axis_value - 2 < 0 && x_axis_value - 2 < 7
-  #   knight_moves(move_8_oclock(current_step), target, path)
-  # else
-  #   knight_moves(move_4_oclock(current_step), target, path)
+  elsif x_axis_difference < 0 && y_axis_difference < 0
+    if x_axis_value - 2 > 0 && y_axis_value - 1 > 0
+    knight_moves(move_8_oclock(current_step), target, path)
+    elsif x_axis_value + 2 < 7 && y_axis_value - 1 > 0
+    knight_moves(move_4_oclock(current_step), target, path)
+    end
   end
   path
 end
-
+#-------------------------------move functions
 def move_1_oclock(arr)
   arr[0] += 1
   arr[1] += 2
@@ -97,9 +103,8 @@ def move_11_oclock(arr)
   arr
 end
 
-
+#---------------------------test area
 p knight_moves([0,0],[3,3])
 p knight_moves([0,0],[7,7])
 p knight_moves([3,3],[0,0])
 p knight_moves([7,7],[0,0])
-
