8168d21a4ae9425f319215dda168ae70a3facd5a
mconley91 <matthew.e.conley@gmail.com>
2025-05-07 17:50:44 -0400

    updating current directionless WIP. Reviewing other's code for direction

--- lib/knight.rb
+++ lib/knight.rb
@@ -5,7 +5,7 @@ def knight_moves(current_step, target, path = [])
   y_axis_difference = target[1] - current_step[1]
   x_axis_value = current_step[0]
   y_axis_value = current_step[1]
-  return if x_axis_value > 7 || y_axis_value > 7 || x_axis_value < 0 || y_axis_value < 0 #enforcing gameboard boundaries
+  # return if x_axis_value > 7 || y_axis_value > 7 || x_axis_value < 0 || y_axis_value < 0 #enforcing gameboard boundaries
   if x_axis_difference == 0 && y_axis_difference == 0 # arrived to the target
     path << [x_axis_value,y_axis_value]
     return path
@@ -38,18 +38,31 @@ def knight_moves(current_step, target, path = [])
       return
     end
   end
+  # p "Current Step:#{current_step}, Current differences X:#{x_axis_difference} Y:#{y_axis_difference}"
   #------------------------------------------------------------ general increment/decrement logic
-  if x_axis_difference > 0 && y_axis_difference > 0 
+  if x_axis_difference > 0 && y_axis_difference > 0
     if x_axis_value + 2 < 7 && y_axis_value + 1 < 7
       knight_moves(move_2_oclock(current_step), target, path)
     elsif x_axis_value - 2 > 0 && y_axis_value + 1 < 7
       knight_moves(move_10_oclock(current_step), target, path)
     end
-  elsif x_axis_difference < 0 && y_axis_difference < 0
+  elsif x_axis_difference < 0 && y_axis_difference < 0 || x_axis_difference == 0
     if x_axis_value - 2 > 0 && y_axis_value - 1 > 0
-    knight_moves(move_8_oclock(current_step), target, path)
+      knight_moves(move_8_oclock(current_step), target, path)
     elsif x_axis_value + 2 < 7 && y_axis_value - 1 > 0
-    knight_moves(move_4_oclock(current_step), target, path)
+      knight_moves(move_4_oclock(current_step), target, path)
+    end
+  elsif x_axis_difference != 0
+    if x_axis_value + 1 < 7 && y_axis_value + 2 < 7
+      knight_moves(move_1_oclock(current_step), target, path)
+    elsif x_axis_value - 1 > 0 && y_axis_value + 2 < 7
+      knight_moves(move_11_oclock(current_step), target, path)
+    end
+  elsif y_axis_difference != 0
+    if x_axis_value - 1 > 0 && y_axis_value - 2 > 0
+      knight_moves(move_7_oclock(current_step), target, path)
+    elsif x_axis_value + 1 < 7 && y_axis_value - 2 > 0
+      knight_moves(move_5_oclock(current_step), target, path)
     end
   end
   path
@@ -108,3 +121,5 @@ p knight_moves([0,0],[3,3])
 p knight_moves([0,0],[7,7])
 p knight_moves([3,3],[0,0])
 p knight_moves([7,7],[0,0])
+p knight_moves([3,3],[4,3])
+p knight_moves([4,3],[3,3])
