072cd6b432f445cde19f2599dba798f163d7e09f
mconley91 <matthew.e.conley@gmail.com>
2025-05-07 15:10:45 -0400

    incrementing towards target works

--- lib/knight.rb
+++ lib/knight.rb
@@ -5,14 +5,14 @@ def knight_moves(current_step, target, path = [])
   y_axis_difference = target[1] - current_step[1]
   x_axis_value = current_step[0]
   y_axis_value = current_step[1]
+  return if x_axis_value > 7 || y_axis_value > 7 || x_axis_value < 0 || y_axis_value < 0 #enforcing gameboard boundaries
   if x_axis_difference == 0 && y_axis_difference == 0 # --------------arrived to the target
     path << [x_axis_value,y_axis_value]
+    # p path
+    # p "X:#{x_axis_value} Y:#{y_axis_value}"
     return path
   else
     path << [x_axis_value,y_axis_value]
-    # p current_step
-    p path
-    p "X:#{x_axis_value} Y:#{y_axis_value}"
     #-------------------------------------------------------finishing moves
     if x_axis_difference == 2 && y_axis_difference == 1
       knight_moves(move_2_oclock(current_step), target, path)
@@ -34,17 +34,19 @@ def knight_moves(current_step, target, path = [])
   end
   #------------------------------------------------------------ general increment/decrement logic
   if x_axis_difference > 0 && y_axis_difference > 0 
-    if y_axis_value + 2 < 7 && x_axis_value + 2 < 7
-      knight_moves(move_1_oclock(current_step), target, path)
-    else
+    if x_axis_value + 2 < 7 && y_axis_value + 1 < 7
       knight_moves(move_2_oclock(current_step), target, path)
+    elsif x_axis_value - 2 > 0 && y_axis_value + 1 < 7
+      knight_moves(move_10_oclock(current_step), target, path)
     end
     
-  elsif x_axis_difference < 0 && y_axis_difference < 0
-    knight_moves(move_7_oclock(current_step), target, path)
+  # elsif x_axis_difference < -2 && y_axis_difference < -1
+  #   if y_axis_value - 2 < 0 && x_axis_value - 2 < 7
+  #   knight_moves(move_8_oclock(current_step), target, path)
+  # else
+  #   knight_moves(move_4_oclock(current_step), target, path)
   end
-
-  "end reached"
+  path
 end
 
 def move_1_oclock(arr)
@@ -96,6 +98,8 @@ def move_11_oclock(arr)
 end
 
 
-
-# p knight_moves([7,7],[0,0])
+p knight_moves([0,0],[3,3])
 p knight_moves([0,0],[7,7])
+p knight_moves([3,3],[0,0])
+p knight_moves([7,7],[0,0])
+
